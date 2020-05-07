7cb7389a922223853cf378394fa5ffe89041cccb
Maarten Esser <maartenjr.esser@gmail.com>
2020-05-07 14:24:28 +0200

    initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,3 +3,13 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "bootstrap";
+import "jquery";
+import "lity";
+import AOS from "aos";
+// import "./scripts";
+
+AOS.init({
+    disable: 'mobile',
+    duration: 600,
+    once: true
+});
