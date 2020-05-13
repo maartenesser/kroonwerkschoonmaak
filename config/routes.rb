6014f9775630d63cb776ba1a1cfae09ce926ee36
Maarten Esser <maartenjr.esser@gmail.com>
2020-05-13 17:41:23 +0200

    Added FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  # resources :pages, only: [:home, :faq]
+  get "faq", to: 'pages#faq', as: :faq
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
