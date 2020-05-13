ccd238c7cdc0932af1b8a4bb1d64282dbd2ba1e8
Maarten Esser <maartenjr.esser@gmail.com>
2020-05-13 17:40:48 +0200

    Added onscroll functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,18 @@
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+require("channels");
 
 import "bootstrap";
 import "jquery";
 import "lity";
 import AOS from "aos";
+import onscroll from "./onscroll";
+
 
 AOS.init({
     disable: 'mobile',
     duration: 600,
     once: true
 });
+
+onscroll();
