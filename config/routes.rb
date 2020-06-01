7cb45dd7a0b0abbf8ca16e536eab83dd12db71bf
Maarten Esser <maartenjr.esser@gmail.com>
2020-06-01 08:55:41 +0200

    added sitemap to application for google

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get "sitemap", to:'sitemaps#index', defaults: {format: :xml}
   # resources :pages, only: [:home, :faq]
   get "faq", to: 'pages#faq', as: :faq
 
   resources :interested_users, only: [:new, :create]
-  get "new", to: "interested_users#new", as: :form
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
